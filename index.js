d40ac86d55bc10bc44a71c5b6776049e893a019c
Karthik Jayaraman <kjram@Karthiks-MacBook-Pro.local>
2025-03-12 14:52:58 +0530

    Remove the tools

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const ULTRAVOX_API_KEY = "81mYuyOH.nYoDrwpOIvxdxEpiCLDEHx2XzD3O8mGu"
 const ULTRAVOX_API_URL = 'https://api.ultravox.ai/api/calls';
 
 // Ultravox configuration
-const SYSTEM_PROMPT = 'Your name is Anjali. You are an employee of Jio in India. You are receiving a phone call. Ask them their name and see how they are doing. You have to answer all the questions they ask you in a patient and friendly manner. You also know about Jio and its services. Keep your answers and conversations crisp, concise and to the point.You are speaking with the user over voice, so do not use lists, emojis or other things. Additionally, do not use action verbs or stage directions in your replies. Find order info by calling a tool orderLookup. Find customer info by calling a tool customerLookup. Find information on Jio FAQs by calling a tool queryCorpus. You can also ask the user to repeat or clarify their question if you do not understand it.';
+const SYSTEM_PROMPT = 'Your name is Anjali. You are an employee of Jio in India. You are receiving a phone call. Ask them their name and see how they are doing. You have to answer all the questions they ask you in a patient and friendly manner. You also know about Jio and its services. Keep your answers and conversations crisp, concise and to the point.You are speaking with the user over voice, so do not use lists, emojis or other things. Additionally, do not use action verbs or stage directions in your replies. Find order info by calling a tool orderLookup. Find customer info by calling a tool customerLookup. Find information on Jio FAQs by calling a tool queryCorpus. Do not tell about which tools you are using internally when talking to a customer.';
 
 const ULTRAVOX_CALL_CONFIG = {
     systemPrompt: SYSTEM_PROMPT,
@@ -19,10 +19,12 @@ const ULTRAVOX_CALL_CONFIG = {
     voice: 'Anjali-Hindi-Urdu',
     temperature: 0.3,
     firstSpeaker: 'FIRST_SPEAKER_AGENT',
-    medium: { "twilio": {} },
-    selectedTools:[
-        { toolName: "orderLookup" },
-    ]
+    medium: { "twilio": {} }
+    // selectedTools:[
+    //     { toolName: "orderLookup" }, 
+    //     { toolName: "customerLookup" }, 
+    //     { toolName: "queryCorpus" }
+    // ]
 };
 
 // Create Ultravox call and get join URL
